b2fb835b60edd9da555abd343a32ff0931de78a9
Ansh <anshrathod29@gmail.com>
2021-06-08 23:50:31 -0700

    fixed bugs

--- lib/screens/comic_search/cubit/searchcomics_cubit.dart
+++ lib/screens/comic_search/cubit/searchcomics_cubit.dart
@@ -14,7 +14,7 @@ class SearchcomicsCubit extends Cubit<SearchcomicsState> {
     emit(state.copyWith(status: ComicStatus.loading));
     try {
       var comicCreaters = Uri.parse(
-          'https://gateway.marvel.com/v1/public/comics?ts=1&apikey=&hash=fcbd875beb64e407e41ea8088ed2cd0c&limit=100&titleStartsWith=$value');
+          'https://gateway.marvel.com/v1/public/comics?ts=1&apikey=690e3ac16286c2de4591eca37269eedb&hash=fcbd875beb64e407e41ea8088ed2cd0c&limit=100&titleStartsWith=$value');
 
       final response = await http.get(comicCreaters);
       final data = json.decode(response.body);
